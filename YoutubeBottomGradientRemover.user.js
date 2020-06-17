8ad4c141dbf133412f3aaf0cd2fc0398eced9a83
Krystian Leśniak <kago56@gmail.com>
2020-06-17 18:19:22 +0200

    Init version 0.1.1

---
+++ YoutubeBottomGradientRemover.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         YouTube bottom gradient remover
+// @namespace    https://github.com/KrystianLesniak
+// @version      0.1.1
+// @license MIT
+// @description  UserScript that disables bottom gradient for YouTube player, all for yours better clarity.
+// @author       Krystian Leśniak
+// @match  *://*.youtube.com/watch?v=*
+// @updateURL   https://github.com/KrystianLesniak/YouTubeBottomGradientRemover/raw/master/YouTubeBottomGradientRemover.user.js
+// @downloadURL https://github.com/KrystianLesniak/YouTubeBottomGradientRemover/raw/master/YouTubeBottomGradientRemover.user.js
+// @supportURL https://github.com/KrystianLesniak/YouTubeBottomGradientRemover/issues
+// ==/UserScript==
+
+(function() {
+    // That's it :))
+
+    fireGradientBottomChange();
+
+    new ResizeObserver(fireGradientBottomChange).observe(document.getElementsByClassName("video-stream html5-main-video")[0]);
+
+    function fireGradientBottomChange(){
+       document.getElementsByClassName("ytp-gradient-bottom")[0].style.height = document.getElementsByClassName("ytp-chrome-bottom")[0].offsetHeight + "px";
+       document.getElementsByClassName("ytp-gradient-bottom")[0].style.paddingTop = 0;
+    }
+
+})();
