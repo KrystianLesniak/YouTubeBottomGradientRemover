b3e8b21de171b40ec106c2f505ac6bf88181383d
Krystian Leśniak <kago56@gmail.com>
2021-10-11 21:18:07 +0200

    Update YouTubeBottomGradientRemover.user.js
    Used @match instead of @include as it worked better in this particular case

--- YouTubeBottomGradientRemover.user.js
+++ YouTubeBottomGradientRemover.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         YouTube bottom gradient remover
 // @namespace    https://github.com/KrystianLesniak
-// @version      0.1.2
+// @version      0.1.3
 // @license MIT
 // @description  UserScript that disables bottom gradient for YouTube player, all for yours better clarity.
 // @author       Krystian Leśniak
-// @match  *://*.youtube.com/watch?*
+// @match *://www.youtube.com/*
 // @updateURL   https://github.com/KrystianLesniak/YouTubeBottomGradientRemover/raw/master/YouTubeBottomGradientRemover.user.js
 // @downloadURL https://github.com/KrystianLesniak/YouTubeBottomGradientRemover/raw/master/YouTubeBottomGradientRemover.user.js
 // @supportURL https://github.com/KrystianLesniak/YouTubeBottomGradientRemover/issues
